073ddf9fb391edf5fad256452732e50e425ed9e5
Gurunath <gurunathkulkarni96@gmail.com>
2023-12-10 15:24:34 +0530

    updaetd

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
-// server.js
 const express = require('express');
-const ffmpeg = require('fluent-ffmpeg');
-const http = require('http');
-const WebSocket = require('ws');
-const fs = require('fs');
-
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
-const port = process.env.PORT || 3001;
-const wsPort = 9999;
-const rtspUrl = 'rtsp://admin:admin123@192.168.1.240:554'; // Replace with your RTSP URL
+const { proxy, scriptUrl } = require('rtsp-relay')(app);
 
-// Set up WebSocket server
-wss.on('connection', (ws) => {
-  console.log('WebSocket connected');
+const handler = proxy({
+  url: `rtsp://admin:admin123@192.168.1.240:554`,
+  // if your RTSP stream need credentials, include them in the URL as above
+  verbose: false,
+});
 
-  // Convert RTSP to HLS using ffmpeg
-  const ffmpegCommand = ffmpeg(rtspUrl)
-    .inputOptions('-rtsp_transport', 'tcp')
-    .outputOptions('-hls_time', '2')
-    .outputOptions('-hls_list_size', '5')
-    .outputOptions('-start_number', '1')
-    .outputOptions('-hls_base_url', `http://localhost:${port}/stream1/`)
-    .output('./public/stream1/index.m3u8')
-    .on('end', () => {
-      console.log('Conversion finished successfully.');
-    })
-    .on('error', (err) => {
-      console.error('Error during conversion:', err);
+// the endpoint our RTSP uses
+app.ws('/api/stream', handler);
+
+// this is an example html page to view the stream
+app.get('/', (req, res) =>
+  res.send(`
+  <canvas id='canvas'></canvas>
+
+  <script src='${scriptUrl}'></script>
+  <script>
+    loadPlayer({
+      url: 'ws://' + location.host + '/api/stream',
+      canvas: document.getElementById('canvas')
     });
+  </script>
+`),
+);
 
-  ffmpegCommand.run();
-
-  // Send HLS URL to the client
-  const hlsUrl = `http://localhost:${port}/stream1/index.m3u8`;
-  ws.send(JSON.stringify({ hlsUrl }));
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(2000);
